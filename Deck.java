b99748f99f299d3ebf87776c2a8f5a755d9cbf59
Samip Thakkar <samipthakkar@users.noreply.github.com>
2018-11-06 23:11:02 -0500

    Final edit of Deck.java and GamePlay.java

---
+++ Deck.java
@@ -0,0 +1,83 @@
+package app.team21.risk.gamemodule;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import app.team21.risk.elements.Card;
+import app.team21.risk.elements.Country;
+
+/**hello bhailu
+ * Allows the creation of the Risk deck containing the 42 Risk cards.
+ *
+ * @author Yash Sheth
+ */
+public class Deck{
+
+    private int i;
+
+    private String input;
+    private String name;
+
+    private String[] typesArray;
+    public ArrayList<Card> deck;
+    private Card drawCard;
+
+   /**
+    *  Creates all cards, one for each territory. Each card has either
+     * a type of Infantry, Cavalry, or Artillery.
+    * @param countries list of country names
+    */
+    public Deck(List<Country> countries) {
+
+        Collections.shuffle(countries);
+
+        //Types of cards
+        typesArray = new String[]{"Infantry", "Cavalry", "Artillery"};
+
+        deck = new ArrayList<Card>();
+
+        for (i = 0; i < countries.size(); i++) {
+            // Add new cards to deck
+            deck.add(new Card(typesArray[i % 3], countries.get(i)));
+           
+        }
+        Collections.shuffle(deck);
+    }
+
+    /**
+     * Public default constructor to access other methods.
+     */
+    public Deck(){
+        deck = new ArrayList<Card>();
+    }
+
+    /**
+     * Removes a card from the deck and return it
+     * @return card object
+     */
+    public Card draw() {
+
+        drawCard = deck.get(0);
+        deck.remove(0);
+
+        return drawCard;
+    }
+
+   /**
+    * Add a card to the deck
+    * @param card name of the card which is to be added to deck
+    */
+    public void add(Card card) {
+
+        deck.add(card);
+    }
+
+   /**
+    * Shuffle the deck of cards
+    */
+    public void shuffle() {
+
+        Collections.shuffle(deck);
+    }
+}
