d79d00ba159c0752d56687aba4fefd840f8fbba5
harshvaghani43 <43417570+harshvaghani43@users.noreply.github.com>
2018-11-08 17:37:49 -0500

    Checked documentation of main test suit.

--- RiskTeam21/src/test/TestSuite.java
+++ RiskTeam21/src/test/TestSuite.java
@@ -1,16 +1,13 @@
 package test;
 
-/**
- * @author Yash Sheth
- *
- */
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import test.team21.risk.elements.*;
 import test.team21.risk.mapmodule.*;
 import test.team21.risk.gamemodule.*;
+
+
 
 @RunWith(Suite.class)
 
@@ -22,6 +19,13 @@ import test.team21.risk.gamemodule.*;
 	
 })
 
+/**
+ * Last Updated on : 06/11/2018, Tuesday
+ * This is a main test suit class.
+ *
+ * @author Yash Sheth
+ * @version 2.0.0
+ */
 public class TestSuite {
 
 }
